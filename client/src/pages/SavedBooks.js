620df519d394128f0b9934842ed4792c1ddcff19
erikolvera <118490997+erikolvera@users.noreply.github.com>
2023-06-08 19:57:36 -0500

    debigging

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -40,37 +40,37 @@ const SavedBooks = () => {
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (!loading) {
     return <h2>LOADING...</h2>;
   }
 
+  const userData = data?.me || {};
+  
   return (
     <>
-      <div fluid className="text-light bg-dark p-5">
-        <Container>
-          <h1>Viewing saved books!</h1>
-        </Container>
-      </div>
       <Container>
-        <h2 className='pt-5'>
-          {userData.savedBooks.length
+        <h2>
+          {userData.savedBooks?.length
             ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
         <Row>
-          {userData.savedBooks.map((book) => {
+          {userData.savedBooks?.map((book) => {
             return (
-              <Col md="4">
-                <Card key={book.bookId} border='dark'>
-                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+              <Col xs={12} md={6} lg={4} key={book.bookId}>
+                <Card className="mb-2">
                   <Card.Body>
                     <Card.Title>{book.title}</Card.Title>
-                    <p className='small'>Authors: {book.authors}</p>
+                    <p className="small">Authors: {book.authors}</p>
                     <Card.Text>{book.description}</Card.Text>
-                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                    <Button
+                      className="btn-block btn-danger"
+                      onClick={() => handleDeleteBook(book.bookId)}
+                    >
                       Delete this Book!
                     </Button>
                   </Card.Body>
+                  <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant="top" />
                 </Card>
               </Col>
             );
@@ -80,5 +80,43 @@ const SavedBooks = () => {
     </>
   );
 };
+
+
+//   return (
+//     <>
+//       <div fluid className="text-light bg-dark p-5">
+//         <Container>
+//           <h1>Viewing saved books!</h1>
+//         </Container>
+//       </div>
+//       <Container>
+//         <h2 className='pt-5'>
+//           {userData.savedBooks?.length
+//             ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+//             : 'You have no saved books!'}
+//         </h2>
+//         <Row>
+//           {userData.savedBooks?.map((book) => {
+//             return (
+//               <Col md="4">
+//                 <Card key={book.bookId} border='dark'>
+//                   {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+//                   <Card.Body>
+//                     <Card.Title>{book.title}</Card.Title>
+//                     <p className='small'>Authors: {book.authors}</p>
+//                     <Card.Text>{book.description}</Card.Text>
+//                     <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+//                       Delete this Book!
+//                     </Button>
+//                   </Card.Body>
+//                 </Card>
+//               </Col>
+//             );
+//           })}
+//         </Row>
+//       </Container>
+//     </>
+//   );
+// };
 
 export default SavedBooks;
